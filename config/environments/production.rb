8c74e3f657c52f89f4a0c6e7f01713e1bf649425
jtorreggiani <joetorreggiani@gmail.com>
2025-01-24 19:48:24 -0500

    Fix linter errors.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,7 +48,7 @@ Rails.application.configure do
 
   # Replace the default in-process memory cache store with a durable alternative.
   # config.cache_store = :solid_cache_store
-  config.cache_store = :redis_cache_store, { url: ENV['HEROKU_REDIS_JADE'] }
+  config.cache_store = :redis_cache_store, { url: ENV["HEROKU_REDIS_JADE"] }
 
   # Replace the default in-process and non-durable queuing backend for Active Job.
   config.active_job.queue_adapter = :solid_queue
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -15,7 +15,7 @@ module ActiveSupport
       Current.session = session
       request = ActionDispatch::Request.new(Rails.application.env_config)
       cookies = request.cookie_jar
-      cookies.signed[:session_id] = {value: session.id, httponly: true, same_site: :lax}
+      cookies.signed[:session_id] = { value: session.id, httponly: true, same_site: :lax }
     end
   end
 end
